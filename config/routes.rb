d73be22de18cb9f1d8a6800ee5499392e80803fa
nickjwill <nicholasjwilliams1@gmail.com>
2017-12-05 20:30:42 -0700

    created custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
-  get 'pages/faq'
+  
+  get 'faqs', to: 'pages#faq'
 
   resources :afternoons
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
 end
