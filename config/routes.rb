fb90bf5ccb57d716a7bd5b5f368f78d67a95c5a8
nickjwill <nicholasjwilliams1@gmail.com>
2017-12-07 19:36:32 -0700

    Changed routes of user login, register

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   get 'faqs', to: 'pages#faq'
 
   resources :afternoons
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,17 @@
-10.times do |afternoon|
-	Afternoon.create!(
-		title: "My Trivia Quiz #{afternoon}",
-		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+14.times do |topic|
+	Topic.create!(
+		title: "Topic #{topic}"
 	)
 end
 
-puts "10 afternoon posts created"
+puts "13 Topics created"
+
+10.times do |afternoon|
+	Afternoon.create!(
+		title: "My Trivia Quiz #{afternoon}",
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+	topic.id: Topic.last.id 
+	)
+end
+
+puts "10 afternoons posts created"
